62cd3edb7bdbef4f9ea28f29bfa4df0f0589b8c7
ActuallyCode <68920391+ActuallyCode@users.noreply.github.com>
2022-02-05 18:11:38 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+$('.main').t({
+    mistype: 7,
+    pause_on_click: true,
+    caret: '<span style="color:hotpink;">▌</span>',
+    typing: function (elm, chr) {
+        if (chr.match(/skyblue/))
+            elm.find('.t-caret').hide();
+        else if (chr.match(/__caret/))
+            elm.find('.t-caret').show();
+    }
+});
